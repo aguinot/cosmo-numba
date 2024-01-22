80db735f28fdc4a1fb8de17833f0b7c270484698
aguinot <axel.guinot.astro@gmail.com>
2024-01-22 13:38:22 -0500

    Add doc

--- cosmo_numba/B_modes/schneider2022.py
+++ cosmo_numba/B_modes/schneider2022.py
@@ -1,3 +1,11 @@
+""" E/B decomposition
+
+Compute E-/B-modes decomposition based on Schneider et al. 2022
+(https://arxiv.org/abs/2110.09774)
+
+Author: Axel Guinot
+
+"""
 import numba as nb
 import numpy as np
 
@@ -13,26 +21,32 @@ from cosmo_numba.math.integrate.quad import interp_quad
     ),
     fastmath=True,
 )
-def H_p(t, t_int, B, t_bar):
+def H_p(t, t_int, theta_bar, B):
+    """H_p
+
+    See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 46.
+
+    Parameters
+    ----------
+    t : float64
+        theta (fancy)
+    t_int : numpy.ndarray(float64)
+        theta on which the integral is performed
+    theta_bar : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+    B : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+
+    Returns
+    -------
+    numpy.ndarray(float64)
+        H_plus
+    """
     return 1/(8*B**3) * \
-        (4*B**2 + 3*((t/t_bar)**2 - 1 - B**2) * ((t_int/t_bar)**2 - 1 - B**2))
-
-
-@nb.njit(
-    nb.float64[:](
-        nb.float64,
-        nb.float64[:],
-        nb.float64,
-        nb.float64
-    ),
-    fastmath=True,
-)
-def H_m(t, t_int, B, t_bar):
-    return (1-B)**2/(8*B**3) * \
         (
-            3*(1-B)**2 * ((1+B)**4-(1+4*B+B**2)*(t/t_bar)**2) *
-            (t_int/t_bar)**(-2) +
-            (3*(1+B)**2*(t/t_bar)**2-(3+6*B+14*B**2+6*B**3+3*B**4))
+            4*B**2
+            + 3*((t/theta_bar)**2 - 1 - B**2)
+            * ((t_int/theta_bar)**2 - 1 - B**2)
         )
 
 
@@ -45,8 +59,33 @@ def H_m(t, t_int, B, t_bar):
     ),
     fastmath=True,
 )
-def K_p(t, t_int, B, t_bar):
-    return (t_bar/t)**2 * H_m(t, t_int, B, t_bar)
+def H_m(t, t_int, theta_bar, B):
+    """H_m
+
+    See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 47.
+
+    Parameters
+    ----------
+    t : float64
+        theta (fancy)
+    t_int : numpy.ndarray(float64)
+        theta on which the integral is performed
+    theta_bar : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+    B : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+
+    Returns
+    -------
+    numpy.ndarray(float64)
+        H_minus
+    """
+    return (1-B)**2/(8*B**3) * \
+        (
+            3*(1-B)**2 * ((1+B)**4-(1+4*B+B**2)*(t/theta_bar)**2) *
+            (t_int/theta_bar)**(-2) +
+            (3*(1+B)**2*(t/theta_bar)**2-(3+6*B+14*B**2+6*B**3+3*B**4))
+        )
 
 
 @nb.njit(
@@ -58,9 +97,62 @@ def K_p(t, t_int, B, t_bar):
     ),
     fastmath=True,
 )
-def K_m(t, t_int, B, t_bar):
-    return (1-B**2)**2 * t_bar**4/(t**2*t_int**2) * \
-        H_p((1-B**2)*t_bar**2/t, (1-B**2)*t_bar**2/t_int, B, t_bar)
+def K_p(t, t_int, theta_bar, B):
+    """K_p
+
+    See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 51.
+
+    Parameters
+    ----------
+    t : float64
+        theta (fancy)
+    t_int : numpy.ndarray(float64)
+        theta on which the integral is performed
+    theta_bar : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+    B : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+
+    Returns
+    -------
+    numpy.ndarray(float64)
+        K_plus
+    """
+    return (theta_bar/t)**2 * H_m(t, t_int, B, theta_bar)
+
+
+@nb.njit(
+    nb.float64[:](
+        nb.float64,
+        nb.float64[:],
+        nb.float64,
+        nb.float64
+    ),
+    fastmath=True,
+)
+def K_m(t, t_int, theta_bar, B):
+    """K_m
+
+    See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 54.
+
+    Parameters
+    ----------
+    t : float64
+        theta (fancy)
+    t_int : numpy.ndarray(float64)
+        theta on which the integral is performed
+    theta_bar : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+    B : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+
+    Returns
+    -------
+    numpy.ndarray(float64)
+        K_minus
+    """
+    return (1-B**2)**2 * theta_bar**4/(t**2*t_int**2) * \
+        H_p((1-B**2)*theta_bar**2/t, (1-B**2)*theta_bar**2/t_int, B, theta_bar)
 
 
 @nb.njit(
@@ -80,6 +172,35 @@ def K_m(t, t_int, B, t_bar):
     fastmath=True,
 )
 def get_S(theta_int, xip_int, xim_int, t, tmin, tmax, theta_bar, B):
+    """get_S
+
+    See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 44 for
+    S_plus and Eq. 45 for S_minus.
+
+    Parameters
+    ----------
+    theta_int : numpy.ndarray(float64)
+        theta used to cimpute the integrals in arcmin
+    xip_int : numpy.ndarray(float64)
+        xi_plus used to compute the integrals
+    xim_int : numpy.ndarray(float64)
+        xi_minus used to compute the integrals
+    t : float64
+        theta (fancy).
+    tmin : float64
+        lower bound used for theta in the integrals
+    tmax : float64
+        upper bound used for theta in the integrals
+    theta_bar : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+    B : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+
+    Returns
+    -------
+    tuple(float64, float64)
+        S_plus, S_minus
+    """
 
     d_theta_int = np.mean(np.diff(np.log(theta_int)))
 
@@ -134,6 +255,35 @@ def get_S(theta_int, xip_int, xim_int, t, tmin, tmax, theta_bar, B):
     fastmath=True,
 )
 def get_V(theta_int, xip_int, xim_int, t, tmin, tmax, theta_bar, B):
+    """get_V
+
+    See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 50 for
+    V_plus and Eq. 53 for V_minus.
+
+    Parameters
+    ----------
+    theta_int : numpy.ndarray(float64)
+        theta used to cimpute the integrals in arcmin
+    xip_int : numpy.ndarray(float64)
+        xi_plus used to compute the integrals
+    xim_int : numpy.ndarray(float64)
+        xi_minus used to compute the integrals
+    t : float64
+        theta (fancy).
+    tmin : float64
+        lower bound used for theta in the integrals
+    tmax : float64
+        upper bound used for theta in the integrals
+    theta_bar : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+    B : float64
+        See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 7.
+
+    Returns
+    -------
+    tuple(float64, float64)
+        V_plus, V_minus
+    """
 
     d_theta_int = np.mean(np.diff(np.log(theta_int)))
 
@@ -198,6 +348,36 @@ def get_pure_EB_modes(
     theta_int, xip_int, xim_int,
     tmin, tmax
 ):
+    """get_pure_EB_modes
+
+    Computes pure xi E-/B-modes decomposition.
+    See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 42-43 for
+    xi_plus and Eq. 55-56 for xi_minus.
+
+    Parameters
+    ----------
+    theta : numpy.ndarray(float64)
+        theta in arcmin
+    xip : numpy.ndarray(float64)
+        xi_plus
+    xim : numpy.ndarray(float64)
+        xi_minus
+    theta_int : numpy.ndarray(float64)
+        theta used to cimpute the integrals in arcmin
+    xip_int : numpy.ndarray(float64)
+        xi_plus used to compute the integrals
+    xim_int : numpy.ndarray(float64)
+        xi_minus used to compute the integrals
+    tmin : float64
+        lower bound used for theta in the integrals
+    tmax : float64
+        upper bound used for theta in the integrals
+
+    Returns
+    -------
+    tuple(float64, float64, float64, float64, float64, float64)
+        xi_plus_E, xi_minus_E, xi_amb_E, xi_plus_B, xi_minus_B, xi_amb_B
+    """
 
     n_theta = len(theta)
     xip_E = np.empty(n_theta, dtype=np.float64)
@@ -302,6 +482,35 @@ def get_CNPT_EB_modes(
     theta_int, xip_int, xim_int,
     tmin, tmax
 ):
+    """get_pure_EB_modes
+
+    Computes the CNPT xi E-/B-modes decomposition.
+    See Schneider et al. 2022 (https://arxiv.org/abs/2110.09774) Eq. 59.
+
+    Parameters
+    ----------
+    theta : numpy.ndarray(float64)
+        theta in arcmin
+    xip : numpy.ndarray(float64)
+        xi_plus
+    xim : numpy.ndarray(float64)
+        xi_minus
+    theta_int : numpy.ndarray(float64)
+        theta used to cimpute the integrals in arcmin
+    xip_int : numpy.ndarray(float64)
+        xi_plus used to compute the integrals
+    xim_int : numpy.ndarray(float64)
+        xi_minus used to compute the integrals
+    tmin : float64
+        lower bound used for theta in the integrals
+    tmax : float64
+        upper bound used for theta in the integrals
+
+    Returns
+    -------
+    tuple(float64, float64, float64, float64)
+        xi_plus_E, xi_minus_E, xi_plus_B, xi_minus_B
+    """
 
     n_theta = len(theta)
     xip_E = np.empty(n_theta, dtype=np.float64)
